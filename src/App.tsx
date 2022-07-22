26ef5f7ff052309d7a644e051a6eeafbb20ace4b
Nandy Bâ <nandyba@yahoo.fr>
2022-07-22 21:32:46 +0200

    base Moralis React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useMoralis } from "react-moralis";
 
 function App() {
+
+  const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      // add your logic here
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAuthenticated]);
+
+  const login = async () => {
+    if (!isAuthenticated) {
+
+      await authenticate({signingMessage: "Log in using Moralis" })
+        .then(function (user) {
+          console.log("logged in user:", user);
+          console.log(user!.get("ethAddress"));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+  const logOut = async () => {
+    await logout();
+    console.log("logged out");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Moralis Hello World!</h1>
+      <button onClick={login} disabled={isAuthenticated}>Moralis Metamask Login</button>
+      <button onClick={logOut} disabled={!isAuthenticated}>Logout</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { MoralisProvider } from "react-moralis";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider serverUrl="https://qrewvab57tnp.usemoralis.com:2053/server" appId="zLoU1HL3sK1G9aDqkRUNSN5oApIBy8kFfYJn40Ba">
+      <App />
+    </MoralisProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
