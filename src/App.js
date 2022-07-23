976f600ec12bb5c64f6b9f624a42ab4244876bea
Nandy Bâ <nandyba@yahoo.fr>
2022-07-24 01:59:47 +0200

    Add logout link

--- src/App.js
+++ src/App.js
@@ -11,20 +11,35 @@ const Rooter = () => {
   const location = useLocation();
   const pathSnippets = location.pathname.split('/').filter(i => i);
 
-  return (
-    <div className="demo">
-      <div className="demo-nav">
-        <Link to="/" style={{'margin':'20px'}}>Home</Link>
-        <Link to="/explore" style={{'margin':'20px'}}>Explore</Link>
-        <Link to="/room" style={{'margin':'20px'}}>Create aquisition</Link>
+  const { isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
+
+  const logOut = async () => {
+    await logout();
+    console.log("logged out");
+  };
+
+  if (!isAuthenticated) {
+    return (
+      <Login/>
+    )
+  }else{
+
+    return (
+      <div className="demo">
+        <div className="demo-nav">
+          <Link to="/" style={{'margin':'20px'}}>Home</Link>
+          <Link to="/explore" style={{'margin':'20px'}}>Explore</Link>
+          <Link to="/room" style={{'margin':'20px'}}>Create aquisition</Link>
+          <a onClick={logOut} style={{'margin':'20px'}}>Logout</a>
+        </div>
+        <Routes>
+          <Route path="/explore" element={<Explore/>} />
+          <Route path="/room" element={<Room/>} />
+          <Route path="*" element={<Home/>} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="/explore" element={<Explore/>} />
-        <Route path="/room" element={<Room/>} />
-        <Route path="*" element={<Home/>} />
-      </Routes>
-    </div>
-  );
+    );
+  }
 };
 
 const App: React.FC = () => (
